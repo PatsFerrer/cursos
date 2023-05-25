2d5bf2994e6d19c206704ca49f34daf5e87dc7e6
Bowie <patriciatferreira1@hotmail.com>
2023-05-25 12:33:39 -0300

    js

---
+++ ada_santander_2023/javascript/variaveis.js
@@ -0,0 +1,26 @@
+let idade = 26
+
+console.log(idade)
+
+//Tipos de variáveis (Primitivos)
+
+idade = 30                 // Number
+let numero = 30            // Number
+let altura = 1.77          // Number
+let nome = 'Patricia'      // String
+let estudando = true       // Booleano
+
+console.log(numero, typeof numero)
+console.log(altura, typeof altura)
+console.log(nome, typeof nome)
+console.log(estudando, typeof estudando)
+
+let outra
+console.log(outra)
+
+let nula = null
+console.log(nula)
+
+var variavel = 30  //não utilize o VAR
+
+const constante = 100 // Não muda
