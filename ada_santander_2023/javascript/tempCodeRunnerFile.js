3c20c2e75118359300220d6d0ad18c96db1c4dfe
Bowie <patriciatferreira1@hotmail.com>
2023-05-26 05:30:32 -0300

    tst

--- ada_santander_2023/javascript/tempCodeRunnerFile.js
+++ ada_santander_2023/javascript/tempCodeRunnerFile.js
@@ -1,1 +1,10 @@
-const
+if (50 > 100) {
+    console.log('falta um tanto')
+} else {
+    if ('1' == '-1') {
+        console.log('quase la')
+    }
+    if (!null) {
+        console.log('sucesso')
+    }
+}
