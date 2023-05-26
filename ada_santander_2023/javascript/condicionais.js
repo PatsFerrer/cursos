91a6d491871597be353b20963ad5753a50c3260a
Bowie <patriciatferreira1@hotmail.com>
2023-05-26 05:17:26 -0300

    if else

---
+++ ada_santander_2023/javascript/condicionais.js
@@ -0,0 +1,25 @@
+// ESTRUTURAS CONDICIONAIS
+
+const idade = 20
+
+if (idade >= 18) {
+    console.log('vc é maior de idade')
+} else {
+    console.log('vc é menor de idade')
+}
+
+console.clear()
+
+// se media >= 7, aprovado
+// se media < 7 e >= 5, recuperaçao
+// se media < 5, reprovado
+
+let media = 4.999
+
+if (media >= 7) {
+    console.log('Aprovado')
+} else if (media >= 5) {
+    console.log('recuperaçao')
+} else {
+    console.log('reprovado')
+}
