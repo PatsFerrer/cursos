d26cd1f6e4e1da5109af5c9fd27e92a7e9980a92
Bowie <patriciatferreira1@hotmail.com>
2023-05-28 09:58:59 -0300

    Estilização com classes e CSS Modules

--- ada_santander_2023/react/meu-projeto-react/src/MeuContador.jsx
+++ ada_santander_2023/react/meu-projeto-react/src/MeuContador.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import styles from './button.module.css'
 
 export default function MeuContador() {
     const [contador, setContador] = useState(0)
@@ -7,25 +8,11 @@ export default function MeuContador() {
         setContador(contador + 1)
     }
 
-    function diminuir() {
-        setContador(contador - 1)
-    }
-
-    if (contador > 5) {
-        return (
-            <div>
-                <h1>Valor muito alto!</h1>
-                <button onClick={aumentar}>Aumentar</button>
-                <button onClick={diminuir}>Diminuir</button>
-            </div>
-        )
-    }
-
     return (
-        <div>
-            <h1>Meu Contador: {contador}</h1>
-            <button onClick={aumentar}>Aumentar</button>
-            <button onClick={diminuir}>Diminuir</button>
+        <div className='container'>
+            <h1>Meu Contador:</h1>
+            <h3>{contador}</h3>
+            <button className={styles.myButton} onClick={aumentar}>aumentar</button>
         </div>
     )
 }
