5f707bf9792d128aa6f07192a365445bfb4bac44
Bowie <patriciatferreira1@hotmail.com>
2023-05-28 07:36:26 -0300

    react

---
+++ ada_santander_2023/react/meu-projeto-react/src/components/MeuComponente.jsx
@@ -0,0 +1,5 @@
+export default function MeuComponente() {
+    return (
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic ducimus, deleniti molestiae veniam, cumque magnam architecto culpa id nostrum neque porro, rerum tempore adipisci molestias suscipit recusandae accusantium. Voluptatibus, accusantium!</p>
+    )
+  }
