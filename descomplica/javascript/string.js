6b44f65fdf299f0190b1f3fff299790729ace9be
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2023-06-02 18:03:24 -0300

    métodos de strings

---
+++ descomplica/javascript/string.js
@@ -0,0 +1,19 @@
+var nome = "Patricia T Ferrer"
+var x = nome.length // retorna o comprimento de um objeto string
+console.log(x)
+
+resultado = x > 15 ? "Obrigado" : "Por favor, preencha seu nome completo"
+console.log(resultado)
+
+var res = nome.toLocaleUpperCase() // Deixa em caixa alta
+console.log(res)
+
+var jogo = "Real vs Barcelona melhor time!"
+var posicao = jogo.indexOf("Barcelona")  // procura onde começa (posição) uma determida palavra
+console.log(posicao)
+
+var cortado = jogo.slice(8,) // vai cortar a palavra, ex: nos parenteses coloque onde deve começar e onde terminar. Se nao tiver nada após a vígula.. ele vai ate o final
+console.log(cortado)
+
+
+
