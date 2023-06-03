c26102f0afa5b5fc088c34c6a5ed1682682ae674
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2023-06-03 17:36:33 -0300

    if

---
+++ descomplica/javascript/if.js
@@ -0,0 +1,17 @@
+var pais = "Brasil"
+
+if (pais != "Brasil"){
+    console.log("voce é estrangeiro")
+} else {
+    console.log("voce é brasileiro")
+}
+
+var idade = 22
+
+if (idade < 16) {
+    console.log("nao vota")
+} else if (idade < 18 || idade > 65) {
+console.log('voto opcional')
+} else {
+console.log('voto obrigatorio')
+}
