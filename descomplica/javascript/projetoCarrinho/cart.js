6a5d0d95a3730b325f45c8ed08314df773972f9c
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2023-06-15 19:16:36 -0300

    carrinho compra finalizado

--- descomplica/javascript/projetoCarrinho/cart.js
+++ descomplica/javascript/projetoCarrinho/cart.js
@@ -9,6 +9,7 @@ function adicionarItem(item) { //item começa com 0
     valorTotal[item] = Number.parseFloat(valorProduto[item]) * qtd[item]
     quantidade.innerHTML = qtd[item]
     total.innerHTML = valorTotal[item].toFixed(2) //toFixed(2) coloca 2 casas decimais
+    valorCompra()
 }
 
 function removerItem(item) {
@@ -19,5 +20,16 @@ function removerItem(item) {
         quantidade.innerHTML = qtd[item]
         valorTotal[item] = Number.parseFloat(valorProduto[item]) * qtd[item]
         total.innerHTML = valorTotal[item].toFixed(2)
+        valorCompra()
     }
+}
+
+function valorCompra() {
+    var valorTotalCompra = document.getElementById('valorTotalCompra')
+    var valor = 0
+
+    for (var i = 0; i < valorTotal.length; i++){
+        valor += valorTotal[i]
+    }
+    valorTotalCompra.innerHTML = valor.toFixed(2)
 }
